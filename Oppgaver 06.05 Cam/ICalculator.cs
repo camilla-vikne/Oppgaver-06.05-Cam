3f8e1965c0298967fcf2e7a6105ae8b0b3d8495c
camillab09 <camillab09.kodehode@gmail.com>
2024-05-06 13:50:40 +0200

    Add project files.

---
+++ Oppgaver 06.05 Cam/ICalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oppgaver_06._05_Cam
+{
+    public interface ICalculator
+    {
+        double Calculate(double x, double y);
+
+    }
+}
---
+++ Oppgaver 06.05 Cam/Program.cs
@@ -0,0 +1,36 @@
+﻿using Oppgaver_06._05_Cam;
+
+int integer = 1;
+string String = "word";
+double doubleNumber = 2.5;
+
+string[] names = { "Anna", "Ole", "Kari", "Jens" };
+
+List<string> list = new List<string>() { "Hello", "this", "is", "C#"};
+//Console.WriteLine(names);
+//Console.WriteLine(list);
+
+for (int i = 0;i < names.Length; i++)
+{
+    Console.WriteLine(names[i]);
+}
+
+Person person = new Person();
+person.Name = "Camilla";
+Console.WriteLine(person.Name);
+
+Console.WriteLine(AddNumbers(1, 3));
+Console.WriteLine(AddNumbers((int)1.5, 3));
+static int AddNumbers(int a, int b)
+{
+    return a + b;
+}
+
+string phrase = "The quick brown fox";
+string[] words = phrase.Split(' ');
+
+foreach (var word in words)
+{
+    Console.WriteLine(word);
+}
+
